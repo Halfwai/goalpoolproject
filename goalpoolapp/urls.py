4a07238f219f56ebab479b5486f3a612f9e77dbb
Halfwai <wai_sanlee@hotmail.com>
2022-10-26 20:41:53 +0800

    basic pages set up, login and out implimented

--- goalpoolapp/urls.py
+++ goalpoolapp/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'goalpoolapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('register', views.register, name='register'),
+    path('loginuser', views.loginuser, name='loginuser'),
+    path('logoutuser', views.logoutuser, name='logoutuser'),
 ]
--- goalpoolapp/views.py
+++ goalpoolapp/views.py
@@ -1,7 +1,69 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from .models import User, Team, League, Player
 
 # Create your views here.
 def index(request):
-    return render(request, 'goalpoolapp/index.html', {
+    if not request.user.is_authenticated:
+        return render(request, 'goalpoolapp/index.html', {
+            'title': "Welcome",
+    })
+    else:
+        return HttpResponseRedirect(reverse("goalpoolapp:dashboard"))
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'goalpoolapp/register.html')
+    elif request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "goalpoolapp/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "goalpoolapp/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("goalpoolapp:index"))
+
+def loginuser(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("goalpoolapp:index"))
+        else:
+            return render(request, "goalpoolapp/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "goalpoolapp/login.html")
+
+def logoutuser(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("goalpoolapp:index"))
+
+
+def dashboard(request):
+    return render(request, 'goalpoolapp/dashboard.html', {
         'title': "Welcome",
     })
