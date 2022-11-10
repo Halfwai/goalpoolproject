dff425710b1738a0e11a1572d71e8138e9344941
Halfwai <wai_sanlee@hotmail.com>
2022-11-10 22:23:00 +0800

    collected static

---
+++ static/goalpoolapp/draft.js
@@ -0,0 +1,80 @@
+const clubs = [
+    "Arsenal",
+    "Aston Villa",
+    "Bournemouth",
+    "Brentford",
+    "Brighton",
+    "Chelsea",
+    "Crystal Palace",
+    "Everton",
+    "Fulham",
+    "Leicester",
+    "Leeds",
+    "Liverpool",
+    "Man City",
+    "Man Utd",
+    "Newcastle",
+    "Nott'm Forest",
+    "Southampton",
+    "Spurs",
+    "West Ham",
+    "Wolves",
+]
+
+// Draft page loading
+try {
+    let teamselect = document.querySelector("#teamselect")
+    for(let i = 0; i < clubs.length; i++){
+        let team = document.createElement("option")
+        team.value = clubs[i];
+        team.innerHTML = clubs[i];
+        teamselect.appendChild(team)
+    }
+    teamselect.selectedIndex = -1;
+    let leagueid = document.querySelector("#leagueid").value;
+teamselect.addEventListener("change", () => {
+    let playercontainer = document.querySelector("#players")
+    playercontainer.innerHTML = ""
+    fetch('../playersearch', {
+        method: 'PUT',
+        headers: {
+            'X-CSRFTOKEN': Cookies.get('csrftoken'),
+        },
+        body: body = JSON.stringify({
+            "team": teamselect.value,
+            "league": leagueid,
+        })
+    })
+    .then(response => response.json())
+    .then(playerset => {
+        playerset = playerset.players;
+        for(let i = 0; i < playerset.length; i++){
+            let player = document.createElement("p")
+            player.innerHTML = playerset[i].nickname
+            player.classList.add("draftplayer");
+            player.addEventListener("click", () => {
+                if (confirm(`You are about to pick ${playerset[i].firstname} ${playerset[i].surname}. This cannot be undone`)){
+                    fetch('../pickplayer', {
+                        method: 'PUT',
+                        headers: {
+                            'X-CSRFTOKEN': Cookies.get('csrftoken'),
+                        },
+                        body: body = JSON.stringify({
+                            "player": playerset[i],
+                            "league": leagueid,
+                        })
+                    })
+                    .then(response => response.json())
+                    .then(response => {
+                        alert(response.message)
+                        location.reload();
+                    })
+                }
+            })
+            playercontainer.appendChild(player)
+        }
+    })
+})
+} catch {
+    console.log("If player is not the picker in the draft, or the draft is not in progress some Javascript is not loaded")
+}
---
+++ static/goalpoolapp/scripts.js
@@ -0,0 +1,104 @@
+window.addEventListener("scroll", () => {
+    navbar = document.querySelector("#navbar")
+    loginbutton = document.querySelector("#loginbutton")
+    navmenus = document.querySelectorAll(".nav-menu-container")
+    if (this.scrollY == 0){
+        navbar.style.backgroundColor = "#371af500"
+        try {
+            loginbutton.style.backgroundColor = "#371af5"
+            loginbutton.style.color = "white"
+        } catch {
+            // user logged in so login button not present
+        }
+        navmenus.forEach(navmenu => {
+            navmenu.style.backgroundColor = "#371af500"
+        })
+    } else {
+        try {
+            loginbutton.style.backgroundColor = "white"
+            loginbutton.style.color = "#371af5"
+        } catch {
+            // user logged in so login button not present
+        }
+        navbar.style.backgroundColor = "#371af5"
+        navmenus.forEach(navmenu => {
+            navmenu.style.backgroundColor = "#371af5"
+        })
+    }
+})
+
+try {
+    let startdraftbutton = document.querySelector("#startdraftbutton")
+    var confirmIt = function (e) {
+        if (!confirm("Are you sure you want to start the draft? New player cannot be added once the draft has started.")) e.preventDefault();
+    };
+    startdraftbutton.addEventListener('click', confirmIt, false);
+} catch {
+}
+try {
+    // hamburgermenu for tablets and phones
+    const menuButton = document.querySelector("#hamburgericon");
+    const closeButton = document.querySelector("#closeicon");
+    const dropdown = document.querySelector("#hamburgermenu");
+    // opens menu when hamburger icon clicked and replaces hamburger icon with close menu icon 
+    menuButton.addEventListener('click', () => {
+        dropdown.style.top = "0px";
+        menuButton.style.display = "none";
+        closeButton.style.display = "block";
+    });
+    // closes menu when close menu icon clicked and replaces with hamburger icon
+    closeButton.addEventListener('click', () => {
+        dropdown.style.top = "-400px";
+        menuButton.style.display = "block";
+        closeButton.style.display = "none";
+    });
+} catch {
+    console.log("User not logged in")
+}
+
+
+// closes all hamburger menus when window is expanded above 780px, and displays hamburger menu icon when window is shrunk below this.
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 780){
+        dropdown.style.top = "-400px";
+        menuButton.style.display = "none";
+        closeButton.style.display = "none";
+    } else {
+        menuButton.style.display = "block";
+    }
+});
+
+try {
+    let draftmenuopen = document.querySelector("#draftmenudown")
+    let draftmenuclose = document.querySelector("#draftmenuup")
+    draftmenuopen.addEventListener("click", () => {
+        let draftmenus = document.querySelector("#draftleagueitems")
+        draftmenus.style.display = "block";
+        draftmenuopen.style.display = "none";
+        draftmenuclose.style.display = "block";
+    })
+    draftmenuclose.addEventListener("click", () => {
+        let draftmenus = document.querySelector("#draftleagueitems")
+        draftmenus.style.display = "none";
+        draftmenuopen.style.display = "block";
+        draftmenuclose.style.display = "none";
+    })
+} catch {
+    console.log("User not logged in")
+}
+
+const logo = document.querySelector("#site-logo");
+logo.addEventListener("click", () => {
+    location.href = "/";
+});
+
+
+let copybutton = document.querySelectorAll(".copybutton");
+copybutton.forEach(function(button) {
+    button.addEventListener("click", () => {
+        text = button.dataset.value;
+        navigator.clipboard.writeText(text);
+        alert("Leaguecode copied to clipboard")
+    })
+})
+
