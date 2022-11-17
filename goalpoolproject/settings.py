2aaf132d0180d35f3e13ff31569da04a9272818d
Halfwai <wai_sanlee@hotmail.com>
2022-11-17 22:37:45 +0800

    admin added

--- goalpoolproject/settings.py
+++ goalpoolproject/settings.py
@@ -149,3 +149,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = env('email')
 EMAIL_HOST_PASSWORD = env('password')
+EMAIL_USE_TLS = True
+
+# Admin settings
+ADMINS = [('Wai', 'footballgoalpool@gmail.com')]
+
+SERVER_EMAIL = 'footballgoalpool@gmail.com'
