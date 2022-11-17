5381deab883c956b75cd08dec155d069acf8cd18
Halfwai <wai_sanlee@hotmail.com>
2022-11-17 20:47:04 +0800

    tidied up some css

--- goalpoolproject/settings.py
+++ goalpoolproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h!395)65a8s3)s&&%gb-e8z@!#h%_r$=(myd5(39q0u&a#if9e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.footballgoalpool.com',
