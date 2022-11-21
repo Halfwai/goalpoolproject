a68d993d8dee42bcbd7648454cd9214e7ee054a4
Halfwai <wai_sanlee@hotmail.com>
2022-11-21 13:04:33 +0800

    fixed updateing team scores in update.py

--- scripts/update.py
+++ scripts/update.py
@@ -46,12 +46,16 @@ def addPlayerGoals(fixture):
                 print("previous goalscorer updated")
                 events_log[fixture_event['player']['id']] += 1
     for key in events_log.keys():
+        fixture.homescore = 0
+        fixture.awayscore = 0
         player = Player.objects.get(playercode=key)
         player.currentweekgoals = events_log[key]
         if player in fixture.hometeam.players.all():
             fixture.homescorers.add(player)
+            fixture.homescore += player.currentweekgoals
         elif player in fixture.awayteam.players.all():
             fixture.awayscorers.add(player)
+            fixture.awayscore += player.currentweekgoals
         player.save()
         fixture.save()
 
