b012da4830e0441b8fe2cfd6cd90fb9adc7fd061
Halfwai <wai_sanlee@hotmail.com>
2023-02-09 12:54:08 +0800

    secret key hidden

--- goalpoolproject/settings.py
+++ goalpoolproject/settings.py
@@ -26,8 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!395)65a8s3)s&&%gb-e8z@!#h%_r$=(myd5(39q0u&a#if9e'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
